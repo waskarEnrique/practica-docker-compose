f0cf5cf64a9d787c18f8296cc028e9cbc02bc8ef
waskarEnrique <waskarenrique03@gmail.com>
2024-07-13 13:00:18 -0400

    Agregar mi nombre y matricula a index.php

--- web/index.php
+++ web/index.php
@@ -16,5 +16,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Conexión fallida: " . $conn->connect_error);
 }
-echo "¡Conexión exitosa!";
+echo "¡Conexión exitosa!. Waskar Enrique Añil 2022-1506";
 ?>
